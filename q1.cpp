a432357a4994b8fbb6177fbe927f2455c4fd116c
saumyatiwari13 <78549785+saumyatiwari13@users.noreply.github.com>
2022-08-26 15:04:26 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,71 @@
+//Name: Saumya Tiwari
+//Group: 20
+//Roll number: 2010991716
+//Set: 04
+//Q1
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+// Function to find the minimum index of the repeating element
+int findMinIndex(int arr[], int n)
+{
+    int minIndex = n;
+    unordered_set<int> set; // creating an empty set to store array elements
+
+    // traversing the array elements from right to left
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (set.find(arr[i]) != set.end()) // if the element is found before, we update the minimum index
+        {
+            minIndex = i;
+        }
+    
+    // if the element is found for the first time, we insert it into the set
+        else
+        {
+            set.insert(arr[i]);
+        }
+    }
+
+    if (minIndex == n) // invalid input
+    {
+        return -1;
+    }
+    return minIndex; // returns minimum index
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter length of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of an array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int minIndex;
+    try
+    {
+        minIndex = findMinIndex(arr, n);
+    }
+    catch (int error)
+    {
+        cout << "Error: " << error << endl;
+    }
+
+    if (minIndex != n)
+    {
+        cout << "The minimum index of the repeating element is " << minIndex;
+    }
+    else
+    {
+        cout << "Invalid Input";
+    }
+
+    return 0;
+}
+
